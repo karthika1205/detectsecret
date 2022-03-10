12b5af86e3281ed726bc72128947fd85a5f28d66
KARTHIKA K S <karthikaks@KARTHIKAs-MacBook-Pro.local>
2022-03-10 13:49:36 +0530

    New file added

---
+++ newsample.sh
@@ -0,0 +1,5 @@
+USERNAME=‘hbuvjscsbhvkksc’
+PASSWORD=‘edsfuphdkfnkjvfjdnvkzendayaqwerty’ #pragma: allowlist secret
+apikey=“A1234567888665KKRFKDLLDLDDJDNNDMDDM” #pragma: allowlist secret
+echo “password: $PASSWORD”
+echo "username: $USERNAME"
